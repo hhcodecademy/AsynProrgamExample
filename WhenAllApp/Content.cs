75f320a6deb0734a31b036f4e5bf0a8f648f4d2c
Huseyn <h.hasanli@pa.gov.az>
2024-05-21 17:03:08 +0400

    Add project files.

---
+++ WhenAllApp/Content.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhenAllApp
+{
+    internal class Content
+    {
+        public int Len { get; set; }
+        public string Url {  get; set; }
+        public int ThreadId { get; set; }
+    }
+}
---
+++ WhenAllApp/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace WhenAllApp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Main Thread id :"+Thread.CurrentThread.ManagedThreadId);
+
+            List<string> list = new List<string>() {
+             "https://qafqazinfo.az/",
+             "https://oxu.az/?v=1",
+             "https://www.google.com/",
+             "https://www.trendyol.com/"
+
+            };
+            List<Task<Content>> contentTasks = new List<Task<Content>>();
+            list.ToList().ForEach(x =>
+            {
+                contentTasks.Add(getSiteContentAsync(x));
+            });
+
+            var contentData = await Task.WhenAll(contentTasks.ToArray());
+
+            foreach (var item in contentData.ToList())
+            {
+                Console.WriteLine($"Operation Thread Id :{item.ThreadId} Url: {item.Url}  Length: {item.Len}");
+            }
+        }
+        public async static Task<Content> getSiteContentAsync(string url)
+        {
+            var response = await new HttpClient().GetStringAsync(url);
+
+            var content = new Content()
+            {
+                Len = response.Length,
+                ThreadId = Thread.CurrentThread.ManagedThreadId,
+                Url = url
+            };
+            return content;
+
+        }
+    }
+}
